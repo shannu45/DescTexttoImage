739582c00ca3c92d9c85ad03957303c7ebd50d5a
shannu45 <40629977+shannu45@users.noreply.github.com>
2023-10-10 01:58:12 +0530

    Update Conditional Network.py

--- Conditional Network.py
+++ Conditional Network.py
@@ -1,28 +1,11 @@
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
-import os
-import pickle
-import random
-import time
-
 import numpy as np
 import pandas as pd
 import tensorflow as tf
 
-assert tf.__version__.startswith('2')
-
-import PIL
-from PIL import Image
 import matplotlib.pyplot as plt
 import tensorflow.keras.backend as K
 from tensorflow.keras import Input, Model
-from tensorflow.keras.layers import LeakyReLU, BatchNormalization, ReLU, Activation
-from tensorflow.keras.layers import UpSampling2D, Conv2D, Concatenate, Dense, concatenate
-from tensorflow.keras.layers import Flatten, Lambda, Reshape, ZeroPadding2D, add
-from tensorflow.keras.optimizers import Adam
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.layers import LeakyReLU, Lambda
 
 def conditioning_augmentation(x):
 	mean = x[:, :128]
@@ -33,7 +16,7 @@ def conditioning_augmentation(x):
 	return c
 
 def build_ca_network():
-	input_layer1 = Input(shape=(1024,)) #size of the vocabulary in the text data
+	input_layer1 = Input(shape=(1024,))  # Size of Vocabulary
 	mls = Dense(256)(input_layer1)
 	mls = LeakyReLU(alpha=0.2)(mls)
 	ca = Lambda(conditioning_augmentation)(mls)
